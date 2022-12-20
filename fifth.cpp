6b3b54b43bec009312e941b52d1ff439adfe8b30
Devam Doshi <devamdoshi212@gmail.com>
2022-12-20 21:31:48 +0530

    Create fifth.cpp

---
+++ fifth.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n)
+    {
+        int sum=0,a,b,c,N=n;
+        a=n%10;
+        n=n/10;
+        b=n%10;
+        n=n/10;
+        c=n%10;
+        sum=pow(a,3)+pow(b,3)+pow(c,3);
+        if(sum==N)
+        return "Yes";
+        else
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
